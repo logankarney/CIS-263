db9dd31fac0e7bc7de3af1ea5874432dd8390041
George Daudlin <daudling@eos20.cis.gvsu.edu>
2018-03-19 14:09:01 -0400

    Added instantiation of hasher2.

--- project1/src/my_hash.cpp
+++ project1/src/my_hash.cpp
@@ -16,3 +16,12 @@ int My_Hash::hasher1(std::string name){
    return hash % heros.size();
 }
 
+int My_Hash::hasher2(std::string name){
+	int seed = 420;
+	unsigned long hash = 0;
+	for(int i = 0; i < name.length(); i++){
+		hash = name[0] + 27 * name[1] + seed * name[3];
+	}
+   return hash % heros.size();
+}
+
