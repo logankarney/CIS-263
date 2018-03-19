a320421fbded3f875f29029b90cb003371e90ecc
George Daudlin <daudling@eos20.cis.gvsu.edu>
2018-03-19 14:08:05 -0400

    csv reader.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
-#include <sstream>
 #include "my_hash.h"
 #include "superhero.h"
+#include "csv.h"
 
 int main(){
 	//creation of vector
 	//std::vector<Superhero> heros(17011);
 	My_Hash* hash = new My_Hash(17011);
 
-	//http://www.cplusplus.com/forum/general/17771/
-	std::ifstream infile( "include/marvel-wikia-data.csv");	
+	//http://www.cplusplus.com/forum/general/17771/	
+	io::CSVReader<13, trim_chars<' '>> in("marvel-wikia-data.csv");
 	
-	if(infile == NULL){
-		std::cout << "Error loading file" << std::endl;
-		return 1;
-	}	
-
-
-	while(infile){
-		std::string s;
-		
-		//terminates loop when the csv runs out of input lines
-		//sets s to next line
-		if (!getline( infile, s )) 
-			break;
-		
-		//TODO: use s to  input data into superhero vector, add to heros
-		std::cout << s << std::endl << std::endl;
-
-	}
-
-	return 0;
 }
 
 bool insert(const Superhero & s){
