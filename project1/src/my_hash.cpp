e96e5844658f800d3693b06a3847d31a4b996cf1
Logan Karney <karneyl@arch04.cis.gvsu.edu>
2018-03-19 13:09:20 -0400

    Added my_hash.cpp

---
+++ project1/src/my_hash.cpp
@@ -0,0 +1,18 @@
+#include "my_hash.h"
+
+My_Hash::My_Hash(int size){
+        heros = std::vector<Superhero>(size);
+}
+
+My_Hash::~My_Hash(){
+}
+
+int My_Hash::hasher1(std::string name){
+        int seed = 616;
+        unsigned long hash = 0;
+        for(int i = 0; i < name.length(); i++){
+                hash = (hash * seed) + name[i];
+        }
+   return hash % heros.size();
+}
+
