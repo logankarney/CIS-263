f3f14431496c49d5d10751ee61374a85077e69df
Logan Karney <karneyl@arch04.cis.gvsu.edu>
2018-03-20 13:22:04 -0400

    Fixed bug with extra column fields.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -10,7 +10,7 @@ int main(){
 	My_Hash* hash = new My_Hash(17011);
 
 	//Loads the file
-	io::CSVReader<13, io::trim_chars<' '>, io::double_quote_escape<',', '\n'> > in("include/marvel-wikia-data.csv");
+	io::CSVReader<13, io::trim_chars<' '>, io::double_quote_escape<',', '\"'> > in("include/marvel-wikia-data.csv");
 	
 	//Variables in which the columns' data will be stored
 	int page_id;
@@ -18,11 +18,11 @@ int main(){
 	std::string urlslug;
 	std::string id;
 	std::string alignment;
-	std::string eye_color;
-	std::string hair_color;
-	std::string sex;
+	std::string eye_color_s;
+	std::string hair_color_s;
+	std::string sex_s;
 	std::string gsm;
-	std::string alive;
+	std::string alive_s;
 	int appearances;
 	std::string first_appearance;
 	int year;
@@ -30,10 +30,11 @@ int main(){
 	//Reads column header names
 	in.read_header(io::ignore_no_column, "page_id", "name", "urlslug", "ID", "ALIGN", "EYE", "HAIR", "SEX", "GSM", "ALIVE", "APPEARANCES", "FIRST APPEARANCE", "Year");
 
-	while(in.read_row(page_id, name, urlslug, id, alignment, eye_color, hair_color, sex, gsm, alive ,appearances, first_appearance, year)){
+	while(in.read_row(page_id, name, urlslug, id, alignment, eye_color_s, hair_color_s, sex_s, gsm, alive_s,appearances, first_appearance, year)){
 
-		//std::cout << in.next_line() << std::endl;	
-		//std::cout << "NAME: " << name << std::endl;
+		std::cout << in.next_line() << std::endl;	
+		//std::cout << "EYE: " << eye_color_s << std::endl;
+		
 
 	}
 	
