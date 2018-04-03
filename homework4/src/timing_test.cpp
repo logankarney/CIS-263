f92c4c7d4c6d9de292bacfd7c7329215702a2b3d
Logan Karney <karneyl@arch08.cis.gvsu.edu>
2018-04-03 18:29:30 -0400

    Finished adding random data.

--- homework4/src/timing_test.cpp
+++ homework4/src/timing_test.cpp
@@ -26,11 +26,14 @@ int main(int c, char *argv[]){
 void create(int size){
 	std::vector<Celebrity> arry(size);
 
+	std::string crafts[] = {"Actor","Artisan", "GVSU Faculty", "Producer", "Writer", "Astronaut", "Body-Builder", "Stunt-Double", "Superhero", "Supervillain"};
+
 	std::srand(time(NULL));	
 	for(int i = 0; i < abs(arry.size()); i++){
 		
 		std::string name = "";
 		int desire = 0;
+		std::string craft = "";
 		bool met = false;
 			
 		//Makes random name
@@ -42,8 +45,23 @@ void create(int size){
 		//Makes random desire level
 		desire = std::rand() % 11;
 		
+		//Chooses a random craft
+		craft = crafts[std::rand() % sizeof(crafts)/sizeof(crafts[0])];
+
 		//Makes random bool for met
 		met = std::rand() % 2;	
 
+		std::cout << name << std::endl;
+		std::cout << desire << std::endl;
+		std::cout << craft << std::endl;
+		std::cout << met << std::endl << std::endl;
+		
+		//Celebrity *c = new Celebrity(name, desire, craft, met);
+		//arry.push_back(*c);
+		//delete(c);
 	}
+
+	//for(int i = 0; i < abs(arry.size()); i++){
+		//std::cout << arry[1].get_name() << std::endl;
+	//}
 }
